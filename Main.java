6a28aaef15e157f78ef75ecc688bc5f4704bff86
jessicapark <jpark32@umbc.edu>
2017-12-02 12:16:02 -0500

    Added Register class

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        setter.thesetter();
+        ExecutiveVP evp=new ExecutiveVP("Test", "Test1", "", 0);
+        evp.returnSalary();
+        evp.hasBenefit();
+
+
+
+    }
+}
