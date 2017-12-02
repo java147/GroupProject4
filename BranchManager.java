febd9a171502a40f66c7ae3a0a1bed2b8d6628c7
jessicapark <jpark32@umbc.edu>
2017-12-02 12:18:31 -0500

    Added employee class

---
+++ BranchManager.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class BranchManager extends employee {
+    private int salary = 85000;
+    public BranchManager(String firstname, String Lastname, String Gender, int count) {
+        super(firstname, Lastname, Gender, count);
+
+    }
+
+
+}
