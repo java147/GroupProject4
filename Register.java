0267a0c8caee354907b80a3961724e0d73098cd2
jessicapark <jpark32@umbc.edu>
2017-12-02 12:15:39 -0500

    Added Register class

---
+++ Register.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Register extends employee {
+    private int salary = 35000;
+    public Register (String firstname, String Lastname, String Gender, int count) {
+        super (firstname, Lastname, Gender, count);
+    }
+
+
+
+}
