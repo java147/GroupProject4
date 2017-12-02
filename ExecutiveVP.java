52d49c663d295b3ce42993d43836f589ebcbae44
jessicapark <jpark32@umbc.edu>
2017-12-02 12:19:00 -0500

    Added ExecutiveVP class

---
+++ ExecutiveVP.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+public class ExecutiveVP extends employee {
+    private int salary = 950000;
+    public ExecutiveVP(String firstname, String Lastname, String Gender, int count) {
+        super(firstname, Lastname, Gender, count);
+    }
+
+
+
+}
