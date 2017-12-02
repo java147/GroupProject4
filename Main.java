98ff0d23304addf104b547d019e6c4de060f4f72
jessicapark <jpark32@umbc.edu>
2017-12-02 13:42:25 -0500

    Added Switch file to Main class

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main
 {
@@ -12,7 +13,41 @@ public class Main
         evp.returnSalary();
         evp.hasBenefit();
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Employee Level Number from 1 and 4: ");
+
+        String employeeLevel = input.nextLine();
+
+        System.out.println(employeeLevel);
+
+        switch (employeeLevel)
+        {
+            case "1":
+                System.out.println("Register = Low Clearance");
+                break;
+
+            case "2":
+                System.out.println("Store Manager = Low Clearance");
+                break;
+
+            case "3":
+                System.out.println("Branch Manager = Mid Clearance");
+                break;
+
+            case "4":
+                System.out.println("Executive VP = High Clearance");
+                break;
+
+            default:
+                System.out.println("Invalid Entry = No Clearance");
+                System.exit(1);
+        }
 
 
     }
+
+
+
 }
+
