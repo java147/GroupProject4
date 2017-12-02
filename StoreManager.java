e23906ad9bbe6d68a225b6c446a478b562813eeb
jessicapark <jpark32@umbc.edu>
2017-12-02 12:09:33 -0500

    Added StoreManager class

---
+++ StoreManager.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class StoreManager extends employee{
+    private int salary = 60000;
+    public StoreManager(String firstname, String Lastname, String Gender, int count) {
+        super(firstname, Lastname, Gender, count);
+    }
+
+
+}
