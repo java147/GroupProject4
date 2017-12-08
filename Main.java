e38464fd14fd0a610186adbd0c11a7ed2da4a386
jessicapark <jpark32@umbc.edu>
2017-12-07 19:45:40 -0500

    Added method overloading in Employee class

--- Main.java
+++ Main.java
@@ -8,7 +8,6 @@ public class Main
 
     public static void main(String[] args)
     {
-        setter.thesetter();
         ExecutiveVP evp=new ExecutiveVP("Test", "Test1", "", 0);
         evp.returnSalary();
         evp.hasBenefit();
@@ -43,9 +42,34 @@ public class Main
                 System.out.println("Invalid Entry = No Clearance");
                 System.exit(1);
         }
-
-
     }
+
+        public void setter() {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter your first name: ");
+            String firstname = input.next();
+            System.out.print("Enter your last name: ");
+            String lastname = input.next();
+            System.out.print("Enter your gender: ");
+            String gender = input.next();
+            gender = gender.toLowerCase();
+            if (!gender.equals("male") && !gender.equals("female")) {
+                do {
+                    System.out.println("Invalid, enter either male or female: ");
+                    gender = input.next();
+                } while (!gender.equals("male") && !gender.equals("female"));
+            }
+
+            byte count = 1;
+            new employee(firstname, lastname, gender, count);
+            String[] position = new String[]{"Store Manager", "Branch Manager", "Executive VP", "Register"};
+            int index = (new Random()).nextInt(position.length);
+            System.out.println(position[index]);
+            String job = position[index];
+            int salary;
+
+
+        }
 
 
 
