063781b1534f0aefb13d008c5d1311df36ca41df
java147 <34147192+java147@users.noreply.github.com>
2017-12-07 21:04:42 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void setter() {
+    public static employee setter() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your first name: ");
         String firstname = input.next();
@@ -22,32 +22,56 @@ public class Main
         }
 
         byte count = 1;
-        employee user = new employee(firstname, lastname, gender, count);
+        employee emp = new employee(firstname, lastname, gender, count);
         String[] position = new String[]{"Store Manager", "Branch Manager", "Executive VP", "Register"};
         int index = (new Random()).nextInt(position.length);
         System.out.println(position[index]);
         String job = position[index];
         int salary;
 
-
-
+    return emp;
     }
 
     public static void main(String[] args)
-
     {
-      //  ExecutiveVP evp=new ExecutiveVP("Test", "Test1", "", 0);
-      //  evp.returnSalary();
-     // evp.hasBenefit();
+        //  ExecutiveVP evp=new ExecutiveVP("Test", "Test1", "", 0);
+        //  evp.returnSalary();
+        // evp.hasBenefit();
 
-        setter();
+        int random = (int )(Math. random() * 4 + 1);
+
+        employee emp = setter();
+
+        System.out.println(emp.getName());
+
+        switch (random){
+            case 1:
+                Register reg = new Register(emp.getFirstName(), emp.getLastName(), emp.getGender(), 1);
+                break;
+            case 2:
+                StoreManager sm = new StoreManager(emp.getFirstName(), emp.getLastName(), emp.getGender(), 1);
+                break;
+            case 3:
+                BranchManager bm = new BranchManager(emp.getFirstName(), emp.getLastName(), emp.getGender(), 1);
+                break;
+            case 4:
+                ExecutiveVP vp = new ExecutiveVP(emp.getFirstName(), emp.getLastName(), emp.getGender(), 1);
+                break;
+            default:
+
+                break;
+        }
+
+
+
 
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Employee Level Number from 1 and 4: ");
 
         String employeeLevel = input.nextLine();
 
         System.out.println(employeeLevel);
-
 
     }
 
@@ -56,4 +80,3 @@ public class Main
 
 
 }
-
